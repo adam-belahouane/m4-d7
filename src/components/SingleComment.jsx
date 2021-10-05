2bb821454b8ed685766170c6e923dea98b451bf9
adam-belahouane <adam_belahouane@outlook.com>
2021-10-05 16:20:05 +0100

    singlecomment no class

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
-class SingleComment extends React.Component {
-    
-    
-    render() {
-        
-        return (
-            <div key={this.props.id}>
-            <p >{this.props.comment} {this.props.rating}/5</p>
-            </div>
-            )
-  }
-}
+// class SingleComment extends React.Component {
+
+//     render() {
+
+//         return (
+//             <div key={this.props.id}>
+//             <p >{this.props.comment} {this.props.rating}/5</p>
+//             </div>
+//             )
+//   }
+// }
+
+const SingleComment = (props) => {
+  return (
+    <div key={props.id}>
+      <p>
+        {props.comment} {props.rating}/5
+      </p>
+    </div>
+  );
+};
 
 export default SingleComment;
